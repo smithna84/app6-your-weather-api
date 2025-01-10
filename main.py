fb8ee6128cce73c7b69f89ca161cae2eac8218f9
nsmith <nathan.smith@aremedia.com.au>
2025-01-10 15:01:18 +1100

    Day 31: Return temperature for any station and date.

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ app = Flask("__name__")
 def home():
     return render_template("home.html")
 
-@app.route("/api/v1/<station>/<data>")
+@app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pd.read_csv("Weather API/TG_STAID" + str(station).zfill(6) + ".txt")
-    temperature = df.station(date)
-    return render_template("about.html")
+    filename = "Weather API/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
